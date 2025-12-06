d0b3cefef95e3c7fd6fa6ef4b85942c9eefc4c53
Saviska Jayawickrama <SaviMJ@outlook.com>
2025-12-06 16:29:27 +0530

    Remove unnecessary package declarations from user and fine strategy classes

--- User/User-Abstraction/K2559495_User.java
+++ User/User-Abstraction/K2559495_User.java
@@ -1,6 +1,3 @@
-package user;
-
-import user.strategy.K2559495_FineStrategy;
 import java.util.ArrayList;
 import java.util.List;
 
--- User/UserBasedFineCalculation-StrategyPattern/K2559495_FineStrategy.java
+++ User/UserBasedFineCalculation-StrategyPattern/K2559495_FineStrategy.java
@@ -1,5 +1,3 @@
-package user.strategy;
-
 public interface K2559495_FineStrategy {
     double calculateFine(int overdueDays);
 }
